84d797dc26dd26415406f5d26486b1e05b0e0fea
Shiva Charan <shiva-charan.m-s@hpe.com>
2015-11-03 06:26:58 -0600

    JIRA:PARSER-1 -Python script for translation patch set-2
    Signed-off-by: shiva-charan.m-s <shiva-charan.m-s@hp.com>

---
+++ yang2tosca/parser.py
@@ -0,0 +1,42 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import sys
+from lxml import etree
+
+
+var = raw_input("Please enter the YANG file name: ")
+lines = var.split('.')
+
+if lines[1] == 'yaml' or lines[1] == 'yang':
+    print"Its a yaml file"
+    os.system('pyang -f yin '+var + ' -o ' + lines[0]+'.xml')
+else:
+    print "Its a "+lines[1]+" file"
+tree = etree.parse(lines[0]+'.xml')
+
+doc = tree.getroot()
+
+xslt_root = etree.parse('tosca_transformer.xslt')
+
+transform = etree.XSLT(xslt_root)
+
+result_tree = transform(doc)
+output_file = lines[0]+'_tosca.yaml'
+
+f = open(output_file, "w")
+f.write(str(result_tree))
+f.close()
+print "TOSCA file generated with name "+output_file
+os.system('rm '+lines[0]+'.xml')
