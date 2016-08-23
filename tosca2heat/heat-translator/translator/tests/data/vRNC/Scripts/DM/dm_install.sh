7782670571684cc57803a9170f476ab231789bf5
shangxdy <shang.xiaodong@zte.com.cn>
2016-08-23 17:11:21 +0800

    Add deployment scripts for vRNC
    Add deployment scripts for vRNC when deploy vRNC in openstack.
    
    JIRA:PARSER-49
    
    Change-Id: I13092f9939a3fe6710e50e2191a82b8cdc25e3e3
    Signed-off-by: shangxdy <shang.xiaodong@zte.com.cn>
    (cherry picked from commit 434aa2750f4b74fa46711c930ed46bb81a34f34f)

---
+++ tosca2heat/heat-translator/translator/tests/data/vRNC/Scripts/DM/dm_install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "DM installing..." >> ~/vRNC_DM.log
+sleep 3
+echo "DM installed" >> ~/vRNC_DM.log
---
+++ tosca2heat/heat-translator/translator/tests/data/vRNC/Scripts/MM/mm_storage_configure.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "MM storage installing..." >> ~/vRNC_MM.log
+sleep 3
+echo "MM storage installed" >> ~/vRNC_MM.log
