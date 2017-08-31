811d035bd2ddae3c8c746cb1e82d9aad6e16bf7c
shangxdy <shang.xiaodong@zte.com.cn>
2017-08-31 10:22:48 +0800

    Package for funectest
    Modify the install script
    
    JIRA: PARSER-125
    
    Change-Id: Ia43687286fb8ac3b18b816aa5e4928ce9a12beec
    Signed-off-by: shangxdy <shang.xiaodong@zte.com.cn>

--- setup.py
+++ setup.py
@@ -1,10 +1,47 @@
+import os
 import setuptools
+
+from distutils.command.install_data import install_data
+from setuptools.command.build_py import build_py
+from setuptools.command.install import install
 
 try:
     import multiprocessing  # noqa
 except ImportError:
     pass
 
+class Parser_build_py(build_py):
+    """Override build_py to call enum_js."""
+
+    def run(self):
+        print(" ===  Before nfv parser build === ")
+        # self.run_command('xxx')
+        super(Parser_build_py, self).run()
+        print(" ===  After nfv parser build === ")
+
+class Parser_install(install):
+    def run(self):
+        print(" ===  Before nfv parser install === ")
+        super(Parser_install, self).run(self)
+        # Custom stuff here
+        # distutils.command.install actually has some nice helper methods
+        # and interfaces. I strongly suggest reading the docstrings.
+        print(" === After nfv parser install === ")
+
+class Parser_post_install(install_data):
+    def run(self):
+        print(" === Before nfv parser post install data === ")
+        # Call parent
+        super(Parser_post_install, self).run(self)
+        # Execute commands
+        print(" === After nfv parser post install data ===")
+
+
 setuptools.setup(
     setup_requires=['pbr>=2.0.0'],
+    cmdclass={
+        "build_py": Parser_build_py,
+        "install_data": Parser_install,
+        "post_install": Parser_post_install,
+    },
     pbr=True)
--- tosca2heat/tosca-parser/setup.py
+++ tosca2heat/tosca-parser/setup.py
@@ -14,16 +14,58 @@
 # limitations under the License.
 
 # THIS FILE IS MANAGED BY THE GLOBAL REQUIREMENTS REPO - DO NOT EDIT
+import os
 import setuptools
+
+from distutils.command.install_data import install_data
+from setuptools.command.build_py import build_py
+from setuptools.command.install import install
 
 # In python < 2.7.4, a lazy loading of package `pbr` will break
 # setuptools if some other modules registered functions in `atexit`.
 # solution from: http://bugs.python.org/issue15881#msg170215
+
+
 try:
     import multiprocessing  # noqa
 except ImportError:
     pass
 
+class Toscaparser_build_py(build_py):
+    """Override build_py to call enum_js."""
+
+    def run(self):
+        print(" ===  Before nfv toscaparser build === ")
+        # self.run_command('xxx')
+        super(Toscaparser_build_py, self).run()
+        print(" ===  After nfv toscaparser build === ")
+
+class Toscaparser_install(install):
+    def run(self):
+        print(" ===  Before nfv toscaparser install === ")
+        # Make sure uninstall toscaparser from openstack
+        os.system("pip uninstall -y tosca-parser")
+
+        super(Toscaparser_install, self).run(self)
+        # Custom stuff here
+        # distutils.command.install actually has some nice helper methods
+        # and interfaces. I strongly suggest reading the docstrings.
+        print(" === After nfv toscaparser install === ")
+
+class Toscaparser_post_install(install_data):
+    def run(self):
+        print(" === Before nfv toscaparser post install data === ")
+        # Call parent
+        super(Toscaparser_post_install, self).run(self)
+        # Execute commands
+        print(" === After nfv toscaparser post install data ===")
+
+
 setuptools.setup(
     setup_requires=['pbr>=2.0.0'],
+    cmdclass={
+        "build_py": Toscaparser_build_py,
+        "install_data": Toscaparser_install,
+        "post_install": Toscaparser_post_install,
+    },
     pbr=True)
